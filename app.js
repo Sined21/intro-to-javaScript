33cd115d3a1e1455d52f2b2a9f26a7a259acf729
denys.havrylenko <denys.havrylenko@pmi.bet>
2023-02-28 23:45:30 +0200

    complete dz-3

---
+++ app.js
@@ -0,0 +1,18 @@
+'use strict';
+
+// Task 1
+let admin;
+let name;
+
+name = 'John';
+admin = name;
+
+alert(admin);
+
+// Task 2
+let planetName;
+let currentUser;
+ 
+// Task 3
+let number = '10' + 10;
+console.log(typeof number);
