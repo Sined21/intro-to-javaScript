e1003efade0a325641e027c4949ab4845a091c34
denys.havrylenko <denys.havrylenko@pmi.bet>
2023-03-04 18:47:52 +0200

    complete dz-5

---
+++ js/app.js
@@ -0,0 +1,31 @@
+'use strict';
+
+// Task #1 
+let user = {};
+
+user.name = 'John';
+user.surname = 'Smith';
+user.name = 'Pete';
+delete user.name;
+
+// Task #2
+// Yes, can be changed
+
+// Task #3
+let salaries = {
+    John: 100,
+    Ann: 160,
+    Pete: 130
+}
+
+function sumSalaries(value) {
+    let sum = null;
+    
+    for (let item of Object.values(value)) {
+        sum += item;
+    }
+    
+    return sum;
+}
+
+alert(sumSalaries(salaries));
