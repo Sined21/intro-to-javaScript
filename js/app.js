263e1b2368006b61849030a35e8ed152ae49f8b1
denys.havrylenko <denys.havrylenko@pmi.bet>
2023-03-12 00:05:38 +0200

    complete dz-5

--- js/app.js
+++ js/app.js
@@ -1,1 +1,24 @@
 'use strict';
+// Task# 1
+// 1. 2
+// 2. 'sub 0'
+// 3. 1456
+// 4. -69
+// 5. 12
+
+// Task# 2
+function getSeconds () {
+    return prompt('Enter number of hours') * 60 * 60;
+}
+
+alert(getSeconds());
+
+// Task# 3
+let num = 1;
+num += 12;
+num -= 14;
+num *= 5;
+num /= 7;
+num++;
+num--;
+alert(num);
