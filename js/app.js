96f3e3da9ab6fb23a3dba0fc6a7cc0a1d8c46cc5
denys.havrylenko <denys.havrylenko@pmi.bet>
2023-03-12 00:35:55 +0200

    complete dz-9

--- js/app.js
+++ js/app.js
@@ -1,1 +1,22 @@
 'use strict';
+
+// 1
+function getEquality(a,b) {
+    return a === b
+}
+
+console.log(getEquality(10, 10));
+
+// 2
+function getGreater (a, b) {
+    return a + b > 10 
+}
+
+console.log(getGreater(1, 1));
+
+// 3
+function getNegative (a) {
+    return a < 0
+}
+
+console.log(getNegative(-1));
