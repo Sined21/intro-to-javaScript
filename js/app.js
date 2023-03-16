ab4cc89902bae0c2464bd589bf73ea53cd23703f
denys.havrylenko <denys.havrylenko@pmi.bet>
2023-03-16 19:09:25 +0200

    complete dz-9

--- js/app.js
+++ js/app.js
@@ -1,22 +1,78 @@
 'use strict';
 
-// 1
-function getEquality(a,b) {
-    return a === b
-}
+let a = +prompt('Enter first number');
+let b = +prompt('Enter second number');
 
-console.log(getEquality(10, 10));
+// 1
+const result1 = (a === 0) ? 'true' : 'false';
+alert(result1);
 
 // 2
-function getGreater (a, b) {
-    return a + b > 10 
-}
-
-console.log(getGreater(1, 1));
+const result2 = (a > 0) ? 'true' : 'false';
+alert(result2);
 
 // 3
-function getNegative (a) {
-    return a < 0
+const result3 = (a < 0) ? 'true' : 'false';
+alert(result3);
+
+// 4
+const result4 = (a >= 0) ? 'true' : 'false';
+alert(result4);
+
+// 5
+const result5 = (a <= 0) ? 'true' : 'false';
+alert(result5);
+
+// 6
+const result6 = (a !== 0) ? 'true' : 'false';
+alert(result6);
+
+// 7
+const result7 = (a == 'test') ? 'true' : 'false';
+alert(result7);
+
+// 8
+const result8 = (a === '1') ? 'true' : 'false';
+alert(result8);
+
+// 9
+const result9 = (a > 0 && a < 5) ? 'true' : 'false';
+alert(result9);
+
+// 10
+const result10 = (a === 0 || a === 2) ? a+=7 : a/=10;
+alert(result10);
+
+// 11
+const result11 = (a <= 1 && b >= 3) ? a + b : a - b;
+alert(result11);
+
+// 12
+const result12 = ((a > 2 && a < 11) || (b >= 6 && b < 14)) ? 'true' : 'false';
+alert(result12);
+
+// 13
+const num = +prompt('enter a number from 1 to 4');
+let result = null;
+
+switch (num) {
+    case 1:
+        result = 'Winter';
+        break;
+        
+    case 2:
+        result = 'Spring';
+        break;
+        
+    case 3:
+        result = 'Summer';
+        break;
+        
+    case 4:
+        result = 'Autumn';
+        break;
+        
+    default: alert('Invalid value entered');
 }
 
-console.log(getNegative(-1));
+alert(result);
