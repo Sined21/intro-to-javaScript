480315b4ce45c38f6af77abc73c4008a8464b7bc
denys.havrylenko <denys.havrylenko@pmi.bet>
2023-03-19 04:01:06 +0200

    complete dz-13

--- js/app.js
+++ js/app.js
@@ -1,1 +1,29 @@
 'use strict';
+const number = +prompt('Enter the number');
+
+if (isNaN(+number) || number === null) {
+    alert('Entered invalid value');
+} else {
+    let label;
+    
+    if (number >= 11 && number < 21 ) {
+        label = 'лет';
+    } else {
+        switch (number % 10) {
+            case 1:
+                label = 'год';
+                break;
+
+            case 2:
+            case 3:
+            case 4:
+                label = 'года';
+                break;
+
+            default:
+                label = 'лет';
+        }
+    }
+    
+    alert(`${number} ${label}`);
+}
