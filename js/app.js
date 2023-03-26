95797180c3ca8bdc9caa5f6bde733e6c0bcb03ea
denys.havrylenko <denys.havrylenko@pmi.bet>
2023-03-27 00:40:50 +0300

    complete dz-18

--- js/app.js
+++ js/app.js
@@ -1,1 +1,18 @@
 'use strict';
+
+const arr = [1, 2, 3, -1, -2, -3];
+
+const getPositiveNumbers = (arr) => {
+    if (!Array.isArray(arr)) return null;
+    if (arr.length === 0) return 'Array is empty';
+    
+    const sortedArray = [];
+    
+    for (let i = 0; i < arr.length; i++) {
+        if (arr[i] > 0) sortedArray.push(arr[i]);
+    }
+
+    return (sortedArray.length !== 0) ? sortedArray : null;
+}
+
+console.log(getPositiveNumbers(arr));
