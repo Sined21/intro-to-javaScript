b0087f694e90510afeca8dd427e7adb4bfd70963
denys.havrylenko <denys.havrylenko@pmi.bet>
2023-03-27 01:05:33 +0300

    complete dz-19

--- js/app.js
+++ js/app.js
@@ -1,1 +1,13 @@
 'use strict';
+const calcProducts = (litersCount) => {
+    if (typeof litersCount !== 'number' || !isFinite(litersCount)) return 'invalid value';
+    
+    const COUNT = 4;
+    const WEIGHT = 0.75;
+    const PRICE = 13;
+    const totalSum = Math.ceil(litersCount * COUNT * WEIGHT * PRICE);
+    
+    return `Итого: ${totalSum}грн за ${litersCount}кг`;
+}
+
+console.log(calcProducts(2));
