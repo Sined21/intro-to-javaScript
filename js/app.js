5f48c1b5a575034af2e2d517a045e1c1d5e6d0fb
denys.havrylenko <denys.havrylenko@pmi.bet>
2023-03-26 22:16:23 +0300

    complete dz-16

--- js/app.js
+++ js/app.js
@@ -1,1 +1,14 @@
 'use strict';
+
+const number = prompt('Enter the number');
+const power = prompt('Enter the power to be raised');
+const result = getPow(number, power);
+
+console.log(result);
+
+function getPow(num, pow = 1) {
+    if (isNaN(num) || num === null) return 'invalid number';
+    if (isNaN(pow) || pow === null) return 'invalid power';
+
+    return Math.pow(num, pow);
+}
