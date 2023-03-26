457f0c69fc32fac4cdd14ffe9715468f259b2893
denys.havrylenko <denys.havrylenko@pmi.bet>
2023-03-27 00:00:04 +0300

    complete dz-17

--- js/app.js
+++ js/app.js
@@ -1,1 +1,20 @@
 'use strict';
+
+function padString (str, length, symbol, right = true) {
+    if (typeof str !== 'string') return 'invalid string value';
+    if (isNaN(length)) return 'invalid string length value';
+    
+    const strLength = str.length;
+    
+    if (strLength === length) return str; 
+    if (strLength > length) return str.substring(0, length);
+    
+    if (typeof symbol !== 'string' || symbol.length !== 1) return 'invalid symbol value';
+    if (typeof right !== 'boolean') return 'invalid placement value';
+    
+    const symbolRepeater = symbol.repeat(length - strLength);
+    
+    return right ? str + symbolRepeater : symbolRepeater + str;
+}
+
+console.log(padString('Hello', 8, '*'));
