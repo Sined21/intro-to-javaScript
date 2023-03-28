5e547f190cfef0d052e4f3b2f0d1a8a0bfc86175
denys.havrylenko <denys.havrylenko@pmi.bet>
2023-03-29 01:31:13 +0300

    complete dz-20

--- js/app.js
+++ js/app.js
@@ -1,1 +1,18 @@
 'use strict';
+
+const fib = (n = 1) => {
+    if (n === 0) return 0;
+    
+    let a = 1;
+    let b = 1;
+    
+    for (let i = 3; i <= n; i++) {
+        let c = a + b;
+        a = b;
+        b = c;
+    }
+    
+    return b;
+}
+
+console.log(fib(4));
