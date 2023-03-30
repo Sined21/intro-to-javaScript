e7be3d49deeea16dc7ffd36878b28516b169b3f1
denys.havrylenko <denys.havrylenko@pmi.bet>
2023-03-30 14:25:52 +0300

    fix dz-23

--- js/app.js
+++ js/app.js
@@ -1,32 +1,25 @@
 'use strict';
 
-const mainFunction = (exp, mul, div, mod) => {
+const mainFunction = (callback) => {
     const num1 = prompt('Enter the first number');
     const num2 = prompt('Enter the second number');
-    exp(num1, num2);
-    mul(num1, num2);
-    div(num1, num2);
-    mod(num1, num2);
+    return callback(num1, num2);
 }
 
 const exponentiation = (a, b) => {
-    const result = Math.pow(a, b);
-    alert(result);
+    return Math.pow(a, b);
 }
 
 const multiplay = (a, b) => {
-    const result = a * b;
-    alert(result);
+    return a * b;
 }
 
 const division = (a, b) => {
-    const result = a / b;
-    alert(result);
+    return a / b;
 }
 
 const modulo = (a, b) => {
-    const result = a % b;
-    alert(result);
+    return a % b;
 }
 
-mainFunction(exponentiation, multiplay, division, modulo);
+alert(mainFunction(multiplay));
