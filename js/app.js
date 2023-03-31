454fecc098977ee407fe8b26ae5a9df6a65238b0
denys.havrylenko <denys.havrylenko@pmi.bet>
2023-03-31 03:24:37 +0300

    complete dz-22

--- js/app.js
+++ js/app.js
@@ -1,1 +1,15 @@
 'use strict';
+
+const num = prompt('Enter the number');
+
+function checkPrime(num) {
+    if (num < 1) return NaN;
+    
+    for (let i = 2; i < num; i++) {
+        if (num % i === 0) return i;
+    }
+
+    return 'Number is prime';
+}
+
+console.log(checkPrime(num));
