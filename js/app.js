02941f2f87c239ec084d1e198436770d5ae07bec
denys.havrylenko <denys.havrylenko@pmi.bet>
2023-04-23 04:25:08 +0300

    complete dz-33

--- js/app.js
+++ js/app.js
@@ -1,1 +1,16 @@
 'use strict';
+
+const getSum = () => {
+  let accum = 0;
+
+  return (num) => {
+    accum += num;
+    return accum;
+  };
+};
+
+const sum = getSum();
+
+sum(3);
+sum(5);
+sum(20);
