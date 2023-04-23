8513b5b96ed32ac0e1ecebeef91a8d83e5a709cb
denys.havrylenko <denys.havrylenko@pmi.bet>
2023-04-23 20:14:17 +0300

    complete dz-36

--- js/app.js
+++ js/app.js
@@ -1,1 +1,21 @@
 'use strict';
+
+const calculator = {
+  read() {
+    this.fitstNum = +prompt('Enter the first number:', 0);
+    this.secondNum = +prompt('Enter the second number:', 0);
+    
+    if (Number.isNaN(this.fitstNum)) this.fitstNum = 0;
+    if (Number.isNaN(this.secondNum)) this.secondNum = 0;
+  },
+  sum() {
+    return this.fitstNum + this.secondNum;
+  },
+  mul() {
+    return this.fitstNum * this.secondNum;
+  }
+};
+
+calculator.read();
+console.log(calculator.sum());
+console.log(calculator.mul());
