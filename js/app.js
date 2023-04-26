a19d20d03fde4791b7f3eb11eab0a8a319d3c6e9
denys.havrylenko <denys.havrylenko@pmi.bet>
2023-04-26 04:14:35 +0300

    complete dz-34

--- js/app.js
+++ js/app.js
@@ -1,1 +1,22 @@
 'use strict';
+
+const generate = () => {
+  const arr = [];
+
+  const getNumber = () => {
+    if (arr.length === 100) return NaN;
+
+    const random = Math.floor(Math.random() * 100) + 1;
+
+    if (arr.includes(random)) return getNumber();
+
+    arr.push(random);
+
+    return random;
+  };
+
+  return getNumber;
+};
+
+const generateInstance = generate();
+generateInstance();
