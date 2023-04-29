edbe737da174daecc82b3662a603c97344a5537e
denys.havrylenko <denys.havrylenko@pmi.bet>
2023-04-29 23:27:38 +0300

    complete dz-39

--- js/app.js
+++ js/app.js
@@ -1,1 +1,24 @@
 'use strict';
+
+const list = document.getElementById('ulId');
+const listItems = list.getElementsByTagName('li');
+
+// Task 1
+let itemCount = 0;
+for (const item of listItems) {
+  itemCount++;
+}
+
+console.log(itemCount);
+
+// Task 1 (v2)
+console.log(listItems.length);
+
+// Task 2
+const arrOfInnerText = [];
+
+for (const item of listItems) {
+  arrOfInnerText.push(item.innerText);
+}
+
+console.log(arrOfInnerText);
