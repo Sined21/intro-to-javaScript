f88c6eb0a3f20650aae49e1e55005a20873f95ef
denys.havrylenko <denys.havrylenko@pmi.bet>
2023-04-29 22:52:04 +0300

    complete task 1

--- js/app.js
+++ js/app.js
@@ -1,1 +1,15 @@
 'use strict';
+
+// Task #1
+const getFactorial = (num) => {
+  if (num === 1 || num === 0) {
+    return 1;
+  }
+
+  return num * getFactorial(num - 1);
+};
+
+console.log(getFactorial(5));
+// Task #2
+
+// Task #3
