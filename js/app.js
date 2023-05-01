1f7ddb24462fa806a6040c6aa6a537f9c7ee19dc
denys.havrylenko <denys.havrylenko@pmi.bet>
2023-05-02 00:16:35 +0300

    complete dz-30

--- js/app.js
+++ js/app.js
@@ -1,15 +1,30 @@
 'use strict';
 
-// Task #1
-const getFactorial = (num) => {
-  if (num === 1 || num === 0) {
-    return 1;
-  }
+(function () {
+  // Task #1
+  const getFactorial = (num) => {
+    if (num === 0 || num === 1) return 1;
 
-  return num * getFactorial(num - 1);
-};
+    return num * getFactorial(num - 1);
+  };
 
-console.log(getFactorial(5));
-// Task #2
+  console.log(getFactorial(5));
 
-// Task #3
+  // Task #2
+  const pow = (num, degree) => {
+    if (degree === 1) return num;
+
+    return num * pow(num, --degree);
+  };
+
+  console.log(pow(2, 3));
+
+  // Task #3
+  const sum = (a, b) => {
+    if (b === 0) return a;
+
+    return sum(a + 1, b - 1);
+  };
+
+  console.log(sum(10, 2));
+})();
