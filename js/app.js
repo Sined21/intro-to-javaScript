56000b8e55b0204f388b3f943e85b802a32ed0d4
denys.havrylenko <denys.havrylenko@pmi.bet>
2023-05-02 02:35:37 +0300

    complete dz-32

--- js/app.js
+++ js/app.js
@@ -1,1 +1,24 @@
 'use strict';
+
+(function () {
+  const arr = [1, 2, 3, 4, [5, 6], [7, 8, [10, 2, 22, 43]]];
+
+  const getFlat = (...arg) => {
+    if (arg.length !== 1) {
+      throw new Error('Function accepts only 1 argument, too much arguments provided');
+    }
+
+    const [arr] = arg;
+    const arrResult = [];
+
+    (function flat(arr) {
+      arr.forEach((item) => {
+        Array.isArray(item) ? flat(item) : arrResult.push(item);
+      });
+    })(arr);
+
+    return arrResult;
+  };
+
+  console.log(getFlat(arr));
+})();
