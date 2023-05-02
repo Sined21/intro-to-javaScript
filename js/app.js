7dc7feccd8f4c86485cb89f7e1816cdc66d7a9cf
denys.havrylenko <denys.havrylenko@pmi.bet>
2023-05-02 23:11:22 +0300

    feat: complete dz-37

--- js/app.js
+++ js/app.js
@@ -1,1 +1,30 @@
 'use strict';
+
+(function () {
+  const obj = {
+    name: 'Jhon',
+    age: 20
+  };
+
+  const foo = function (phone, address) {
+    console.log(this.name, this.age, phone, address);
+  };
+
+  foo.bind(obj)('093-444-44-44', '4 Avenue');
+
+  const bind = (fn, ctx, ...rest) => {
+    return (...args) => {
+      const id = Date.now().toString();
+
+      ctx[id] = fn;
+
+      const result = ctx[id](...rest.concat(...args));
+
+      delete ctx[id];
+
+      return result;
+    };
+  };
+
+  bind(foo, obj, '093-333-33-33', '3 Avenue')();
+})();
