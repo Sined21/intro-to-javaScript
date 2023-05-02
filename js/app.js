1af4ccad81a675ca3913276262e7c3bb7af42682
denys.havrylenko <denys.havrylenko@pmi.bet>
2023-05-02 04:21:31 +0300

    complete dz-31

--- js/app.js
+++ js/app.js
@@ -1,1 +1,23 @@
 'use strict';
+
+(function () {
+  const palindrome = () => {
+    let steps = 0;
+
+    const getPalindrome = (num) => {
+      let reverse = parseFloat(num.toString().split('').reverse().join('')) * Math.sign(num);
+      
+      if (num !== reverse) {
+        steps++;
+        return getPalindrome(num + reverse);
+      }
+
+      return { result: num, steps };
+    };
+
+    return getPalindrome;
+  };
+
+  const palindromeInstance = palindrome();
+  console.log(palindromeInstance(96));
+})();
