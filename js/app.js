dc239486c4fc49c1dc9ff2fcba80ee2dc860fdce
denys.havrylenko <denys.havrylenko@pmi.bet>
2023-05-09 18:29:44 +0300

    feat: complete dz-43

--- js/app.js
+++ js/app.js
@@ -1,1 +1,26 @@
 'use strict';
+
+(function () {
+  const createTable = () => {
+    const table = document.createElement('table');
+    let indexNum = 1;
+
+    for (let i = 1; i <= 10; i++) {
+      const tableRow = document.createElement('tr');
+
+      for (let j = 1; j <= 10; j++) {
+        const tableData = document.createElement('td');
+        tableData.textContent = indexNum;
+        indexNum++;
+
+        tableRow.appendChild(tableData);
+      }
+
+      table.appendChild(tableRow);
+    }
+
+    return table;
+  };
+
+  document.body.appendChild(createTable());
+})();
