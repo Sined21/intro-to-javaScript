a897d31c6a860fd11e31f676fa9cde115682e104
denys.havrylenko <denys.havrylenko@pmi.bet>
2023-05-09 16:44:45 +0300

    feat: complete dz-41

--- js/app.js
+++ js/app.js
@@ -1,1 +1,38 @@
 'use strict';
+
+(function () {
+  const ul = document.getElementById('ulId');
+  const li = ul.querySelectorAll('li');
+  const attributeList = ul.attributes;
+  const NAME = 'Denys';
+
+  // Task 1
+  li.forEach((el) => console.log(el));
+
+  // Task 2
+  const attributeValues = [];
+
+  for (let attribute of attributeList) {
+    attributeValues.push(attribute.value);
+  }
+
+  console.log(attributeValues);
+
+  // Task 3
+  const attributeNames = [];
+
+  for (let attribute of attributeList) {
+    attributeNames.push(attribute.name);
+  }
+
+  console.log(attributeNames);
+
+  // Task 4
+  ul.lastElementChild.textContent = `Hello my name is ${NAME}`;
+
+  // Task 5
+  ul.firstElementChild.setAttribute('data-my-name', NAME);
+
+  // Task 6
+  ul.removeAttribute('data-dog-tail');
+})();
