776a4ec6dadb2046bc4a169c669f4a89092b45bb
denys.havrylenko <denys.havrylenko@pmi.bet>
2023-05-17 03:16:16 +0300

    feat: complete dz-44

--- js/app.js
+++ js/app.js
@@ -1,5 +1,15 @@
 'use strict';
 
 (function () {
-  
+  const form = document.getElementById('form');
+
+  form.addEventListener('submit', (event) => {
+    event.preventDefault();
+
+    const formData = new FormData(form);
+    const formValues = Object.fromEntries(formData.entries());
+
+    form.reset();
+    console.log(formValues);
+  });
 })();
