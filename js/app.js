7b12ddec7776263e03e0d57fc65eafa1f690289d
denys.havrylenko <denys.havrylenko@pmi.bet>
2023-06-13 23:55:36 +0300

    feat: complete dz-54

--- js/app.js
+++ js/app.js
@@ -1,1 +1,33 @@
 'use strict';
+
+void (function () {
+  let user = {
+    data: {
+      a: 1,
+      b: 2,
+      c: 3,
+      d: {
+        a1: 1,
+        b1: 2,
+        c1: 3,
+        d1: {
+          a2: 3,
+          b2: 3,
+          c2: 3,
+        },
+      },
+    },
+  };
+
+  const deepFreeze = (obj) => {
+    for (let key in obj) {
+      Object.freeze(obj);
+
+      if (typeof obj[key] === 'object') {
+        deepFreeze(obj[key]);
+      }
+    }
+  };
+
+  deepFreeze(user);
+})();
