db7e8a638030e6a1efb037d61c4c6fe304c679c8
denys.havrylenko <denys.havrylenko@pmi.bet>
2023-07-02 22:38:34 +0300

    complete dz-67

--- js/app.js
+++ js/app.js
@@ -1,1 +1,27 @@
 'use strict';
+
+void (function () {
+  const goods = [
+    ['jacket', 40],
+    ['sweater', 25],
+    ['pants', 25],
+    ['hoodie', 20],
+    ['t-shirt', 15],
+    ['socks', 5],
+    ['briefs', 8],
+  ];
+
+  const goodsTotal = (goods) => {
+    let total = 0;
+    const goodsMap = new Map();
+    goods.forEach((item) => goodsMap.set(item[0], item[1]));
+
+    for (const value of goodsMap.values()) {
+      total += value;
+    }
+
+    return total;
+  };
+
+  goodsTotal(goods);
+})();
