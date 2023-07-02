b200b62256c31ff93783fbd82832527afa7c3602
denys.havrylenko <denys.havrylenko@pmi.bet>
2023-07-03 01:18:19 +0300

    complete dz-68

--- js/app.js
+++ js/app.js
@@ -1,1 +1,17 @@
 'use strict';
+
+void (function () {
+  const str = 'abcccdffuhcscjs686r456';
+
+  const uniqSymbolsCount = (str) => {
+    const strMap = new Set();
+
+    for (let i = 0; i < str.length; i++) {
+      strMap.add(str[i]);
+    }
+
+    return strMap.size;
+  };
+
+  uniqSymbolsCount(str);
+})();
