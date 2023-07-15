5cc33595f9fcdff3f827f34c425a3a6fd24f13cf
denys.havrylenko <denys.havrylenko@pmi.bet>
2023-07-15 18:44:07 +0300

    complete dz-69

--- js/app.js
+++ js/app.js
@@ -1,1 +1,13 @@
 'use strict';
+
+void (function () {
+  const studentIvan = { name: 'Иван Иванов' };
+  const gradesIvan = { math: 90, fiz: 85, eng: 95 };
+
+  const saveGrades = (student, grades) => {
+    const gradesMap = new WeakMap();
+    gradesMap.set(student, grades);
+  };
+
+  saveGrades(studentIvan, gradesIvan);
+})();
