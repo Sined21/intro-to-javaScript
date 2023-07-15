025da812b4beb34e5aeb1106c518e7ee60e636ec
denys.havrylenko <denys.havrylenko@pmi.bet>
2023-07-15 20:03:29 +0300

    complete dz-70

--- js/app.js
+++ js/app.js
@@ -1,1 +1,20 @@
 'use strict';
+
+void (function () {
+  const playerSet = new WeakSet();
+
+  const player1 = { id: 1, name: 'Ivan', level: 10 };
+  const player2 = { id: 1, name: 'Stepan', level: 7 };
+  const player3 = { id: 1, name: 'Ruslan', level: 11 };
+
+  const addPlayerToLevelSet = (player) => {
+    if (player.level < 10) return;
+
+    playerSet.add(player);
+  };
+
+  addPlayerToLevelSet(player1);
+  addPlayerToLevelSet(player2);
+  addPlayerToLevelSet(player3);
+  addPlayerToLevelSet(player1);
+})();
