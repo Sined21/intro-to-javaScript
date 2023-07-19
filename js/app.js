ca41fbedfd0f11508f8d58e495ad60357e29adf6
denys.havrylenko <denys.havrylenko@pmi.bet>
2023-07-20 02:44:52 +0300

    complete dz-71

--- js/app.js
+++ js/app.js
@@ -1,1 +1,21 @@
 'use strict';
+
+void (function () {
+  const students = [
+    { name: 'John', age: 20, subject: 'Math' },
+    { name: 'Alice', age: 22, subject: 'English' },
+    { name: 'Bob', age: 21, subject: 'History' },
+  ];
+
+  const printGreetings = (arr) => {
+    const printValue = [];
+
+    arr.forEach(({ name, subject }) => {
+      printValue.push(`Привет, ${name}! Ты изучаешь предмет ${subject}`);
+    });
+
+    return printValue.join('\n');
+  };
+
+  console.log(printGreetings(students));
+})();
